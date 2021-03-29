66aee6ae95443b478b3f99d5502fcbd8c8fca0cc
Tom MacKenzie <rampantnine@gmail.com>
2021-03-29 13:59:20 -0400

    a

--- kata_timing.py
+++ kata_timing.py
@@ -17,23 +17,18 @@ import hashlib
 # print(test_func(100))
 
 
-# def test_func(n):
-#     start_time = time.time()
-#     for i in range(n):
-#         for j in range(n):
-#             x = i * j
-#     end_time = time.time()
-#     if .09 < end_time - start_time < .11:
-#         return end_time - start_time
+def test_func(n):
+    start_time = time.time()
+    for i in range(n):
+        for j in range(n):
+            x = i * j
+    end_time = time.time()
+    if .09 < end_time - start_time < .11:
+        return end_time - start_time
 
 # Now use timing and some guessing to make a function which consumes just a password and returns a (salt, r, theHash)
 # triplet that took around 1 second to create. Verify that you can recreate theHash.
 
-import hashlib
-import bcrypt
+salt = bcrypt.gensalt
 
-salt = bcrypt.gensalt()
-
-def timing_func(pwd):
-    while time < 1 sec
-
+def one_second_trip(password):
